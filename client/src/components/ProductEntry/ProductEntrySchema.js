92467a0261df83a555e817bf0a16704d029b63ee
apoorvjauhariinfo <apoorv.jauhari@innovyatech.com>
2024-08-23 19:12:17 +0530

    upc code

--- client/src/components/ProductEntry/ProductEntrySchema.js
+++ client/src/components/ProductEntry/ProductEntrySchema.js
@@ -3,7 +3,7 @@ import * as Yup from "yup";
 export const ProductSchema = Yup.object({
   //producttype: Yup.string().required("Select Your Product Type"),
   // category: Yup.string().required("Please Select Product Category"),
-  upccode: Yup.string().min(6).max(20).required("Please Enter UPC Manually"),
+  upccode: Yup.string().min(21).max(20).required("Please Enter UPC Manually"),
   name: Yup.string().min(2).required("Please enter Product Name"),
   // manufacturer: Yup.string().min(6).required("Please enter Manufacturer Name"),
   // emergencytype: Yup.string().required("Please Select Emergency Type"),

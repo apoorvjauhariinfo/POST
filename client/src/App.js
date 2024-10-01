5067a7eb3b12bb42e65ab2a18472a8ef9a5449a2
apoorvjauhariinfo <apoorv.jauhari@innovyatech.com>
2024-10-01 19:14:24 +0530

    AdminBug

--- client/src/App.js
+++ client/src/App.js
@@ -77,6 +77,8 @@ function App() {
           <Route path="/signup" element={<UserRegistration />} />
           <Route path="/login" element={<Login />} />
           <Route path="/" element={<Login />} />
+          <Route path="/adminlogin" element={<AdminLogin />} />
+
           <Route path="/users/:id/verify/:token" element={<EmailVerify />} />
           <Route path="/inventorymanagers/:id" element={<Acceptance />} />
           <Route path="/admins/:id" element={<AdminAcceptance />} />
@@ -129,7 +131,6 @@ function App() {
         <>
           <Route path="/" element={<AdminDashboard />} />
           <Route path="/addadmin" element={<AddAdminScreen />} />
-          <Route path="/adminlogin" element={<AdminLogin />} />
           <Route path="/admindashboard" element={<AdminDashboard />} />
           <Route path="/totalhospital" element={<TotalHospital />} />
           <Route path="/newregistration" element={<NewRegistration />} />
